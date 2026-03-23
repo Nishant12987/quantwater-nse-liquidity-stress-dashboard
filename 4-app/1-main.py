11ec9655be14ecb1858444491a4c2e35eb2b30fa
Nishant12987 <151148726+Nishant12987@users.noreply.github.com>
2026-03-23 09:15:32 +0530

    Update 1-main.py

--- 4-app/1-main.py
+++ 4-app/1-main.py
@@ -4,17 +4,15 @@ from pathlib import Path
 
 
 # =========================
-# PATH FIX (IMPORTANT)
+# ADD ENSEMBLE PATH
 # =========================
 
 BASE = Path(__file__).resolve().parents[1]
-
-# add ensemble folder
 sys.path.append(str(BASE / "3-src/4-ensemble"))
 
 
 # =========================
-# IMPORT AFTER PATH FIX
+# IMPORT
 # =========================
 
 from dashboard_backend import backend_pipeline
@@ -38,7 +36,7 @@ data = backend_pipeline()
 
 
 # =========================
-# UI (TEMP TEST)
+# UI (TEST)
 # =========================
 
 st.title("Liquidity Risk Dashboard")
