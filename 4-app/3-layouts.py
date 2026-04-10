b97546359f1442d34684076c26d051d2f308cbec
Nishant12987 <151148726+Nishant12987@users.noreply.github.com>
2026-04-11 00:31:40 +0530

    Update 3-layouts.py

--- 4-app/3-layouts.py
+++ 4-app/3-layouts.py
@@ -1,1 +1,14 @@
 # Page structure
+import streamlit as st
+
+
+def section_header(title):
+    st.markdown(f"""
+        <h2 style='margin-top:20px;'>
+            {title}
+        </h2>
+    """, unsafe_allow_html=True)
+
+
+def card_container():
+    return st.container()
